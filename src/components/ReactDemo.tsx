a420a4129589fce11ca179927a339e4390935e3e
linyiren <yirenlin0218@gmail.com>
2023-09-27 17:58:50 +0800

    try to add react to koa

---
+++ src/components/ReactDemo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReactDemo1: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello, React in koa!</h1>
+        </div>
+    );
+};
+
+export default ReactDemo1;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,43 +1,66 @@
 import koa from "koa";
 import Router from "koa-router";
 
-import fs from 'fs';
+import fs from "fs";
 import path from "path";
 import axios from "axios";
 import nunjucks from "nunjucks";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
 
-import printObjectDataInColumns from '../utils/printObjectDataInColumns';
+import printObjectDataInColumns from "../utils/printObjectDataInColumns";
+// import ReactDemo1 from "../components/ReactDemo";
+const ReactDemo1 = () => import("../components/ReactDemo");
 
 const userRouter = new Router();
 
 const baseUrl = `https://oss.x-lab.info/open_digger/github/`;
 
-const resultPath = path.join(__dirname, '../../public/result.html');
-const result = nunjucks.compile(fs.readFileSync(resultPath, 'utf-8'));
+const resultPath = path.join(__dirname, "../../public/result.html");
+const result = nunjucks.compile(fs.readFileSync(resultPath, "utf-8"));
 
 // 访问根路径时展示页面
-userRouter.get("/", async(ctx: any, next: any) => {
-    ctx.type = 'html';
-    ctx.body = fs.createReadStream(path.join(__dirname, '../../public/form.html'));
-})
+userRouter.get("/", async (ctx: any, next: any) => {
+    ctx.type = "html";
+    ctx.body = fs.createReadStream(
+        path.join(__dirname, "../../public/form.html")
+    );
+});
+
+userRouter.get("/react", async (ctx: any) => {
+    const reactHtml = ReactDOMServer.renderToString(<ReactDemo1 />: React.FC);
+    ctx.type = "html";
+    ctx.body = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>React Page</title>
+        </head>
+        <body>
+            <div id="app">${reactHtml}</div>
+        </body>
+    </html>
+  `;
+});
 
 // 处理表单提交
-userRouter.post("/submit", async(ctx: any, next: any) => {
+userRouter.post("/submit", async (ctx: any, next: any) => {
     const formData = ctx.request.body;
     const repository = formData.repository;
     const metric = formData.metric;
-    const {data} = await axios.get(baseUrl + `${repository}/${metric}.json`);
+    const { data } = await axios.get(baseUrl + `${repository}/${metric}.json`);
     const dataStr = printObjectDataInColumns(data);
-    const formattedData = dataStr.replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;').replace(/\n/g, '<br/>');
-    // const combinedStr = repository + metric;
+    const formattedData = dataStr
+        .replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;")
+        .replace(/\n/g, "<br/>");
 
-    const html = result.render({dataStr});
-    console.log('data', data)
-    console.log(dataStr)
-    console.log(formattedData)
-    console.log('resultpath', resultPath)
-    ctx.type = 'html';
+    const html = result.render({ dataStr });
+    // console.log('data', data)
+    // console.log(dataStr)
+    // console.log(formattedData)
+    // console.log('resultpath', resultPath)
+    ctx.type = "html";
     ctx.body = html;
-})
+});
 
 export default userRouter;
